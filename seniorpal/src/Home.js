858e61788812a7ef2293d1187486ac15ae78a8c5
Christie <c.leung1234567890@gmail.com>
2022-09-06 10:29:25 -0700

    Added under development page and started structuring website following Figma design
    Signed-off-by: Christie <c.leung1234567890@gmail.com>

---
+++ seniorpal/src/Home.js
@@ -0,0 +1,93 @@
+//import {useState} from "react";
+//import {Link} from "react-router-dom";
+import './Home.css'
+
+function Home() {
+    let inDev = true;
+
+    return (
+        <>
+        {inDev ?
+                <div className={"window-container"} style={{
+                    backgroundColor: "#fcfbfb",
+                    placeContent: "center",
+                    display: "flex"
+                }}>
+                    <div style={{
+                        width: "fit-content",
+                        height: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        paddingRight: "5%",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}>
+                        <img src={"./SenPal Logo.png"} alt={"Logo"} style={{
+                            height: "75vh",
+                            width: "75vh",
+                            paddingTop: "5%",
+                            paddingBottom: "5%",
+                        }}/>
+                        <text style={{
+                            fontSize: "1.4vw",
+                            fontStyle: "normal",
+                            fontWeight: "300"
+
+                        }}>A kind way to send a virtual HUG - Heartfelt, Unique, and Generous.</text>
+                    </div>
+                    <div style={{
+                        width: "calc(100% / 2.5)",
+                        height: "100%",
+                        display: "flex",
+                        alignItems: "center"
+                    }}>
+                        <text style={{
+                            fontSize: "2vw",
+                            fontStyle: "normal",
+                            fontWeight: "300",
+                            lineHeight: "7vh"
+                        }}>
+                            Hello! <br/>
+                            Our website is currently under development. <br/>
+                            We hope to provide you with an aesthetic and detailed website to ensure that
+                            your experience with us is welcoming and warm so please be patient with us!</text>
+                    </div>
+
+                </div> :
+                <div className={"window-container"} style={{backgroundColor: "#F7F2F3"}}>
+                    <img className={"logo"} src={"./SenPal Logo.png"} alt={"Logo"}/>
+                    <div className={"navbar"}>
+                        <text className={"nav-buttons"}>Home</text>
+                        <text className={"nav-buttons"}>About</text>
+                        <text className={"nav-buttons"}>Team</text>
+                        <text className={"nav-buttons"}>FAQ</text>
+                        <text className={"nav-buttons"}>Contact</text>
+                    </div>
+                    <header>hi</header>
+
+                </div>
+        }
+        </>
+    )
+}
+
+export default Home;
+
+/*
+ <div className={"navbar"}>
+                <button className={"nav-buttons"}>Home</button>
+                <Link to={'About'}>
+                    <button className={"nav-buttons"}>About</button>
+                </Link>
+                <Link to={'Team'}>
+                    <button className={"nav-buttons"}>Team</button>
+                </Link>
+                <Link to={'FAQ'}>
+                    <button className={"nav-buttons"}>FAQ</button>
+                </Link>
+                <Link to={'Contact'}>
+                    <button className={"nav-buttons"}>Contact</button>
+                </Link>
+                <text>Hi</text>
+            </div>
+ */

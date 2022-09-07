d6150aa6d1c35103dd7fbee55072f5a9f17b5f65
Christie <c.leung1234567890@gmail.com>
2022-09-06 22:13:14 -0700

    Added mobile detection features
    Signed-off-by: Christie <c.leung1234567890@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,26 @@
 //import {useState} from "react";
 //import {Link} from "react-router-dom";
 import './Home.css'
+import {isMobile} from "react-device-detect";
 
 function Home() {
     let inDev = true;
+    let mobile = false;
 
-    return (
-        <>
-        {inDev ?
+    if (isMobile) {
+        return (
+            <>
                 <div className={"window-container"} style={{
                     backgroundColor: "#fcfbfb",
-                    placeContent: "center",
-                    display: "flex"
+                    display: "flex",
+                    flexDirection: "column",
+                    overflowY: "scroll",
+                    alignItems: "center"
                 }}>
                     <div style={{
-                        width: "fit-content",
-                        height: "100%",
-                        display: "flex",
-                        flexDirection: "column",
+                        paddingTop: "2%",
+                        paddingLeft: "5%",
                         paddingRight: "5%",
-                        alignItems: "center",
-                        justifyContent: "center"
-                    }}>
-                        <img src={"./Logo.png"} alt={"Logo"} style={{
-                            height: "75vh",
-                            width: "75vh",
-                            paddingTop: "5%",
-                            paddingBottom: "5%",
-                        }}/>
-                        <text style={{
-                            fontSize: "1.4vw",
-                            fontStyle: "normal",
-                            fontWeight: "300"
-
-                        }}>A kind way to send a virtual HUG - Heartfelt, Unique, and Generous.</text>
-                    </div>
-                    <div style={{
-                        width: "calc(100% / 2.5)",
-                        height: "100%",
                         display: "flex",
                         alignItems: "center"
                     }}>
@@ -45,15 +28,90 @@ function Home() {
                             fontSize: "2vw",
                             fontStyle: "normal",
                             fontWeight: "300",
-                            lineHeight: "7vh"
+                            lineHeight: "7vh",
+                            padding: "inherit"
                         }}>
                             Hello! <br/>
                             Our website is currently under development. <br/>
                             We hope to provide you with an aesthetic and detailed website to ensure that
-                            your experience with us is welcoming and warm so please be patient with us!</text>
+                            your experience with us is welcoming and warm so please be patient with us!
+                        </text>
                     </div>
+                    <div style={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}>
+                        <img src={"./Logo.png"} alt={"Logo"} style={{
+                            height: "75vw",
+                            width: "75vw",
+                            paddingTop: "5%",
+                            paddingBottom: "5%",
+                        }}/>
+                        <text style={{
+                            fontSize: "1.4vw",
+                            fontStyle: "normal",
+                            fontWeight: "300",
+                            paddingBottom: "10%",
+                        }}>A kind way to send a virtual HUG - Heartfelt, Unique, and Generous.
+                        </text>
+                    </div>
+                </div>
+            </>
+        )
+    } else {
+        return (
+        <>
+            {inDev ?
+                <div className={"window-container"} style={{
+                            backgroundColor: "#fcfbfb",
+                            placeContent: "center",
+                            display: "flex"
+                        }}>
+                            <div style={{
+                                width: "fit-content",
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                paddingRight: "5%",
+                                alignItems: "center",
+                                justifyContent: "center"
+                            }}>
+                                <img src={"./Logo.png"} alt={"Logo"} style={{
+                                    height: "75vh",
+                                    width: "75vh",
+                                    paddingTop: "5%",
+                                    paddingBottom: "5%",
+                                }}/>
+                                <text style={{
+                                    fontSize: "1.4vw",
+                                    fontStyle: "normal",
+                                    fontWeight: "300"
 
-                </div> :
+                                }}>A kind way to send a virtual HUG - Heartfelt, Unique, and Generous.
+                                </text>
+                            </div>
+                            <div style={{
+                                width: "calc(100% / 2.5)",
+                                height: "100%",
+                                display: "flex",
+                                alignItems: "center"
+                            }}>
+                                <text style={{
+                                    fontSize: "2vw",
+                                    fontStyle: "normal",
+                                    fontWeight: "300",
+                                    lineHeight: "7vh"
+                                }}>
+                                    Hello! <br/>
+                                    Our website is currently under development. <br/>
+                                    We hope to provide you with an aesthetic and detailed website to ensure that
+                                    your experience with us is welcoming and warm so please be patient with us!
+                                </text>
+                            </div>
+                        </div> : 
                 <div className={"window-container"} style={{backgroundColor: "#F7F2F3"}}>
                     <img className={"logo"} src={"./Logo.png"} alt={"Logo"}/>
                     <div className={"navbar"}>
@@ -66,9 +124,10 @@ function Home() {
                     <header>hi</header>
 
                 </div>
-        }
+            }
         </>
     )
+    }
 }
 
 export default Home;
